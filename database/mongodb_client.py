31dc7ff30490a1225d1ae8b2e7af4eb37f76ef64
Olena Liubynetska <olena.liuby@gmail.com>
2024-04-05 07:07:59 +0100

    refactor: Inherit MongoDBClient from QThread & implement run method

--- database/mongodb_client.py
+++ database/mongodb_client.py
@@ -1,17 +1,23 @@
 from datetime import datetime
 
-from PyQt6.QtCore import QObject, pyqtSlot
+from PyQt6.QtCore import QThread, pyqtSignal, pyqtSlot
 from motor.motor_asyncio import AsyncIOMotorClient
 from loguru import logger
+import asyncio
 
 
-class MongoDBClient(QObject):
+class MongoDBClient(QThread):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.client = AsyncIOMotorClient("mongodb://localhost:27017/crypto_prices")
         self.db = self.client["crypto_prices"]
         self.collection = self.db["price_updates"]
         self._log = logger.bind(name="MongoDBClient")
+        self.loop = asyncio.new_event_loop()
+
+    def run(self) -> None:
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
 
     @pyqtSlot(dict)
     async def store_price_data(self, data: dict) -> None:
